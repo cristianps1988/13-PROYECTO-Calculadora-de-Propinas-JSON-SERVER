bdf955629ba069531ceb2f5c0081ebe6e1c6c50d
Cristianps1988 <cristianps_1@hotmail.com>
2023-03-25 21:42:23 -0500

    mostrarPlatillos function added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 let cliente = {
     mesa: '',
     hora: '',
-    platillos: []
+    pedido: []
+}
+
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Pasteles'
 }
 
 const btnGuardarCliente = document.querySelector('#guardar-cliente')
@@ -49,5 +55,54 @@ function obtenerPlatillos(){
     const url = 'http://localhost:4000/platillos'
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado))
+        .then(resultado => mostrarPlatillos(resultado))
+}
+
+function mostrarPlatillos(platillos){
+    const contenido = document.querySelector('#platillos .contenido')
+    platillos.forEach(platillo => {
+        const {nombre, precio, categoria, id} = platillo;
+        const row = document.createElement('div')
+        row.classList.add('row', 'py-3', 'border-top')
+
+        const nombrePlatillo = document.createElement('div')
+        nombrePlatillo.classList.add('col-md-4')
+        nombrePlatillo.textContent = nombre
+
+        const precioPlatillo = document.createElement('div')
+        precioPlatillo.classList.add('col-md-3', 'fw-bold')
+        precioPlatillo.textContent = `$${precio}`
+
+        const categoriaPlatillo = document.createElement('div')
+        categoriaPlatillo.classList.add('col-md-3')
+        categoriaPlatillo.textContent = categorias[categoria]
+
+        const inputCantidad = document.createElement('input')
+        inputCantidad.type = 'number'
+        inputCantidad.min = 0
+        inputCantidad.id = `producto-${id}`
+        inputCantidad.value = 0
+        inputCantidad.classList.add('form-control')
+
+        // detectar el cambio de número y asociarlo con el id del platillo que lo genero
+        inputCantidad.onchange = function(){
+            const cantidad = parseInt(inputCantidad.value)
+            agregarPlatillo({...platillo, cantidad})
+        }
+
+        const agregar = document.createElement('div')
+        agregar.classList.add('col-md-2')
+        agregar.appendChild(inputCantidad)
+
+        row.appendChild(nombrePlatillo)
+        row.appendChild(precioPlatillo)
+        row.appendChild(categoriaPlatillo)
+        row.appendChild(agregar)
+
+        contenido.appendChild(row)
+    })
+}
+
+function agregarPlatillo(platillo){
+    console.log(platillo)
 }
